a9a8ea643c1ee357ce7d8092e57bf7ad82a00aa1
divyanshchahar <divyanshchahar@outlook.com>
2022-07-07 12:59:40 +0530

    added top navigation bar

---
+++ src/componenets/topnavbar.js
@@ -0,0 +1,27 @@
+// IMPORTING FUNCTIONALITY
+import React from "react";
+
+// IMPORTING IMAGES
+import usericon from "../assets/images/user-solid.svg";
+import logoicon from "../assets/images/store-solid.svg";
+import carticon from "../assets/images/cart-shopping-solid.svg";
+
+const TopNavBar = () => {
+  return (
+    <div className="topnavbar">
+      <img src={logoicon} alt={""} />
+
+      <input type="text" />
+
+      <button>
+        <img src={carticon} alt="" />
+      </button>
+
+      <button>
+        <img src={usericon} alt="" />
+      </button>
+    </div>
+  );
+};
+
+export default TopNavBar;
