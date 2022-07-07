874e43a06a3fed24e538b0b8182549e5d43a29e7
divyanshchahar <divyanshchahar@outlook.com>
2022-07-07 17:31:02 +0530

    removed unnecesary code from useAPIData.js

--- src/components/useAPIData.js
+++ src/components/useAPIData.js
@@ -3,17 +3,10 @@ import { useState, useEffect, useCallback } from "react";
 const useAPIData = (url) => {
   const [products, setProducts] = useState([]);
 
-  // const getData = useCallback(async () => {
-  //   const response = await fetch(url);
-  //   const data = await response.json();
-  //   setProducts(data);
-  // }, [url]);
-
   const getData = useCallback(async () => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
         setProducts(data);
       })
       .catch((err) => {
