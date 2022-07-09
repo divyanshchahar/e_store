3c96351439c704e34957d4632a13f88e6e6b55ca
divyanshchahar <divyanshchahar@outlook.com>
2022-07-09 17:17:06 +0530

    added functionality for loading single images

---
+++ src/components/singleimageloader.js
@@ -0,0 +1,10 @@
+import image_index from "../assets/data/image_index.js";
+
+const SingleImageLoader = (productId) => {
+  const selectedItem = image_index.filter(
+    (item) => item.id === productId.productId
+  );
+  return <img src={selectedItem[0].images[0]} alt="not found" />;
+};
+
+export default SingleImageLoader;
