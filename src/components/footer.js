84b5a51b2db735329472dd3d6fd63a14b202b31b
divyanshchahar <divyanshchahar@outlook.com>
2022-07-09 13:09:55 +0530

    added code for footer

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <ul>
+        <li>Terms of Use</li>
+        <li>Coookie Pilicy</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
