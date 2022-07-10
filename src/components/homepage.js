576e4df4aece55b0415ad37489a1cef2e1cbbd68
divyanshchahar <divyanshchahar@outlook.com>
2022-07-10 13:54:16 +0530

    added home page

---
+++ src/components/homepage.js
@@ -0,0 +1,15 @@
+import TopNavBar from "./topnavbar.js";
+import ProductList from "./productlist.js";
+import Footer from "./footer.js";
+
+const HomePage = () => {
+  return (
+    <>
+      <TopNavBar />
+      <ProductList />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
