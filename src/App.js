6b4cdd3f11c1f74771c823173bc4fc4530e9e12e
divyanshchahar <divyanshchahar@outlook.com>
2022-07-11 08:47:26 +0530

    integrated routes in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import HomePage from "./components/homepage";
+import PrivacypolicyPage from "./components/privacypolicypage";
+import TermsofUsePAge from "./components/termsofusepage";
+import CookiePolicyPage from "./components/cookiepolicypage";
+import ErrorPage from "./components/error";
+
 function App() {
-  return <h1>TEST</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/privacypolicy" element={<PrivacypolicyPage />} />
+        <Route path="/termsofuse" element={<TermsofUsePAge />} />
+        <Route path="/cookiepolicy" element={<CookiePolicyPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
