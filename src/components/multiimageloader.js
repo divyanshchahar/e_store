d3150c6680695f4467d541e4ea355c005773c94e
divyanshchahar <divyanshchahar@outlook.com>
2022-07-12 18:08:19 +0530

    removed commented code

--- src/components/multiimageloader.js
+++ src/components/multiimageloader.js
@@ -3,12 +3,9 @@ import image_index from "../assets/data/image_index.js";
 const MultiImageLoader = ({ productId }) => {
   const selectedItem = image_index.filter((item) => item.id === productId);
 
-  // return <img src={selectedItem[0].images[0]} alt="not found" />;
   return selectedItem[0].images.map((imgaeItem) => {
     return <img src={imgaeItem} alt="not found" />;
   });
-
-  // console.log(productId);
 };
 
 export default MultiImageLoader;
