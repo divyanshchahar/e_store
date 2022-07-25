027736af3efa04ca0f7bde585aa95979d679cf23
divyanshchahar <divyanshchahar@outlook.com>
2022-07-25 23:49:40 +0530

    added styling for product listing made changes to css changed componenet for product listing

--- src/components/productlist.js
+++ src/components/productlist.js
@@ -13,16 +13,21 @@ const ProductList = () => {
     <>
       {products.map((product) => {
         return (
-          <div key={product.id}>
-            <Link to={`/product/${product.id}`}>
-              <SingleImageLoader productId={product.id} />
-            </Link>
-            <Link to={`/product/${product.id}`}>
-              <h1>{product.name}</h1>
-            </Link>
-            <p>{product.price}</p>
-            <button>Add to Cart</button>
-            <button>Buy Now</button>
+          <div key={product.id} className="product">
+            <div className="productimg">
+              <Link to={`/product/${product.id}`}>
+                <SingleImageLoader productId={product.id} />
+              </Link>
+            </div>
+
+            <div className="productdetail">
+              <Link to={`/product/${product.id}`}>
+                <h1>{product.name}</h1>
+              </Link>
+              <p>{product.price}</p>
+              <button>Add to Cart</button>
+              <button>Buy Now</button>
+            </div>
           </div>
         );
       })}
