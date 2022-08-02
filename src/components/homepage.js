3ed8da734c0b40141ac19ef151299389920ffadc
divyanshchahar <divyanshchahar@outlook.com>
2022-08-02 19:11:27 +0530

    added functionality to recieve arguments

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,20 +1,59 @@
+// IMPORTING FUNCTIONALITY
+import { useState } from "react";
+
 // IMPORTING COMPONENETS
 import TopNavBar from "./topnavbar.js";
 import ProductList from "./productlist.js";
 import Footer from "./footer.js";
 
 const HomePage = () => {
+  const [args, setArgs] = useState("http://localhost:3001/product_data");
   return (
     <>
       <TopNavBar />
       <div className="listofproducts">
         <div className="sortingrow">
-          <button>Sort: Name (A to Z)</button>
-          <button>Sort: Name (Z to A)</button>
-          <button>Sort: Price (Ascending)</button>
-          <button>Sort: Price (Descending)</button>
+          <button
+            onClick={() =>
+              setArgs(
+                "http://localhost:3001/product_data?_sort=name&_order=asc"
+              )
+            }
+          >
+            Sort: Name (A to Z)
+          </button>
+
+          <button
+            onClick={() =>
+              setArgs(
+                "http://localhost:3001/product_data?_sort=name&_order=desc"
+              )
+            }
+          >
+            Sort: Name (Z to A)
+          </button>
+
+          <button
+            onClick={() =>
+              setArgs(
+                "http://localhost:3001/product_data?_sort=price&_order=asc"
+              )
+            }
+          >
+            Sort: Price (Ascending)
+          </button>
+
+          <button
+            onClick={() =>
+              setArgs(
+                "http://localhost:3001/product_data?_sort=price&_order=desc"
+              )
+            }
+          >
+            Sort: Price (Descending)
+          </button>
         </div>
-        <ProductList />
+        <ProductList url={args} />
       </div>
       <Footer />
     </>
