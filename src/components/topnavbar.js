6148274ce4c2e4af222e831270ad76c2004af556
divyanshchahar <divyanshchahar@outlook.com>
2022-08-04 17:55:59 +0530

    added functionality to search for products by name

--- src/components/topnavbar.js
+++ src/components/topnavbar.js
@@ -1,5 +1,5 @@
 // IMPORTING FUNCTIONALITY
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 // IMPORTING ASSETS
@@ -7,14 +7,37 @@ import usericon from "../assets/images/user-solid.svg";
 import logoicon from "../assets/images/store-solid.svg";
 import carticon from "../assets/images/cart-shopping-solid.svg";
 
+//IMPORTING COMPONENTS
+import useAPIData from "./useAPIData";
+
 const TopNavBar = () => {
+  const url = "http://localhost:3001/product_data?_sort=name&_order=asc";
+  const [searched, setSearched] = useState([]);
+
+  const products = useAPIData(url);
+
+  const handleChange = (e) => {
+    if (e.target.value !== "") {
+      const filtered = products.filter((item) => {
+        return item.name.toLowerCase().includes(e.target.value.toLowerCase());
+      });
+      setSearched(filtered);
+    } else setSearched([]);
+  };
+
   return (
     <div className="topnavbar">
       <img src={logoicon} alt={""} />
 
       <div className="searchbar">
-        <input type="text" />
+        <input type="text" onChange={handleChange} />
         <button>Search</button>
+      </div>
+
+      <div className="searchresults">
+        {searched.map((item) => {
+          return <p>{item.name}</p>;
+        })}
       </div>
 
       <div>
