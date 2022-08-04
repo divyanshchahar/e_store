766f58e4d301498b19bf47bcde5ba83f0c3de8c6
divyanshchahar <divyanshchahar@outlook.com>
2022-08-04 19:50:34 +0530

    removed unwanted code

--- src/components/userhandler.js
+++ src/components/userhandler.js
@@ -7,8 +7,6 @@ const UserHandler = () => {
   const [appState] = useAPIData("http://localhost:3001/appdata");
   const { userCount, currentUSer } = appState || {};
 
-  console.log(userCount);
-
   return (
     <>
       {typeof userCount === "undefined" && <h1>Loading...</h1>}
