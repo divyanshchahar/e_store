bda7c8c2f2991bc0fe9013c41862e8fe80a405b6
divyanshchahar <divyanshchahar@outlook.com>
2022-08-05 16:03:34 +0530

    added functionality to list user

--- src/components/userSelectionPage.js
+++ src/components/userSelectionPage.js
@@ -1,5 +1,26 @@
+// IMPORTING COMPONENETS
+import useAPIData from "./useAPIData";
+
 function UserSelectionPage() {
-  return <h1>User Selection Page</h1>;
+  const users = useAPIData("http://localhost:3001/users");
+  return (
+    <>
+      <h1>select User</h1>
+      {users.map((item) => {
+        const {
+          id,
+          personelinfo: { name, email },
+        } = item;
+        return (
+          <div key={id}>
+            <p>{name}</p>
+            <p>{email}</p>
+            <button>Select User</button>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default UserSelectionPage;
