889c0787916e1d0d367d77c3f5a368a687e1b934
divyanshchahar <divyanshchahar@outlook.com>
2022-08-05 17:31:34 +0530

    added functionality to to delete users

--- src/components/userSelectionPage.js
+++ src/components/userSelectionPage.js
@@ -3,6 +3,22 @@ import useAPIData from "./useAPIData";
 
 function UserSelectionPage() {
   const users = useAPIData("http://localhost:3001/users");
+  const appData = useAPIData("http://localhost:3001/appdata");
+
+  function deleteUser(id) {
+    const url = "http://localhost:3001/users/";
+    const deleteUrl = url.concat(id);
+
+    fetch(deleteUrl, {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => response.json)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }
   return (
     <>
       <h1>select User</h1>
@@ -16,6 +32,7 @@ function UserSelectionPage() {
             <p>{name}</p>
             <p>{email}</p>
             <button>Select User</button>
+            <button onClick={() => deleteUser(id)}>Delete User</button>
           </div>
         );
       })}
