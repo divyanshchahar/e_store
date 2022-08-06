590c8a3899cc2e8205cca80a2c5391e6f325fb1a
divyanshchahar <divyanshchahar@outlook.com>
2022-08-06 15:37:22 +0530

    made changes to userhandler.js - changed conged conditional rendering - add dummy page to for editing user data

---
+++ src/components/userEdit.js
@@ -0,0 +1,5 @@
+function UserEdit() {
+  return <h1>User Edit Page</h1>;
+}
+
+export default UserEdit;
--- src/components/userhandler.js
+++ src/components/userhandler.js
@@ -1,17 +1,32 @@
-// IMPORTING ASSETS
+// IMPORTING COMPONENETS
 import useAPIData from "./useAPIData";
 import LoginPage from "./loginPage";
 import UserSelectionPage from "./userSelectionPage";
+import UserEdit from "./userEdit";
 
 const UserHandler = () => {
   const [appState] = useAPIData("http://localhost:3001/appdata");
   const { userCount, currentUSer } = appState || {};
+  let toRender = "";
+
+  if (currentUSer !== null) {
+    toRender = "editPage";
+    console.log("I am here");
+  } else {
+    if (userCount === 0) {
+      toRender = "loginPage";
+    }
+    if (userCount > 0) {
+      toRender = "userSelection";
+    }
+  }
 
   return (
     <>
       {typeof userCount === "undefined" && <h1>Loading...</h1>}
-      {userCount > 0 && <UserSelectionPage />}
-      {userCount === 0 && <LoginPage />}
+      {toRender === "editPage" && <UserEdit />}
+      {toRender === "loginPage" && <LoginPage />}
+      {toRender === "userSelection" && <UserSelectionPage />}
     </>
   );
 };
