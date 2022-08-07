0c97ce55dbe5406ccace6b8996dbce512b8f9a28
divyanshchahar <divyanshchahar@outlook.com>
2022-08-07 15:24:55 +0530

    temporarily removed capability to edit user data

--- src/components/userEdit.js
+++
@@ -1,5 +0,0 @@
-function UserEdit() {
-  return <h1>User Edit Page</h1>;
-}
-
-export default UserEdit;
--- src/components/userhandler.js
+++ src/components/userhandler.js
@@ -2,29 +2,23 @@
 import useAPIData from "./useAPIData";
 import LoginPage from "./loginPage";
 import UserSelectionPage from "./userSelectionPage";
-import UserEdit from "./userEdit";
+// import UserEdit from "./userEdit";
 
 const UserHandler = () => {
   const [appState] = useAPIData("http://localhost:3001/appdata");
   const { userCount, currentUSer } = appState || {};
   let toRender = "";
 
-  if (currentUSer !== null) {
-    toRender = "editPage";
-    console.log("I am here");
+  if (userCount === 0) {
+    toRender = "loginPage";
   } else {
-    if (userCount === 0) {
-      toRender = "loginPage";
-    }
-    if (userCount > 0) {
-      toRender = "userSelection";
-    }
+    toRender = "userSelection";
   }
 
   return (
     <>
       {typeof userCount === "undefined" && <h1>Loading...</h1>}
-      {toRender === "editPage" && <UserEdit />}
+      {/* {toRender === "editPage" && <UserEdit />}/ */}
       {toRender === "loginPage" && <LoginPage />}
       {toRender === "userSelection" && <UserSelectionPage />}
     </>
