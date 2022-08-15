9ef3520a65cd37f22bc44a2def72a48bace5a14c
divyanshchahar <divyanshchahar@outlook.com>
2022-08-15 17:33:25 +0530

    changes page switching behaviour

--- src/pages/userpage.js
+++ src/pages/userpage.js
@@ -1,28 +1,44 @@
+//IMPORTING FUNCTIONALITY
+import { useNavigate } from "react-router-dom";
+
 // IMPORTING COMPONENETS
 import useAPIData from "../services/utils/useAPIData";
 import LoginPage from "../pages/loginPage";
 import UserSelectionPage from "../pages/userSelectionPage";
-// import UserEdit from "./userEdit";
 
 const UserPage = () => {
   const [appState] = useAPIData("http://localhost:3001/appdata");
   const { userCount, currentUSer } = appState || {};
+  const navigate = useNavigate();
   let toRender = "";
 
-  if (userCount === 0) {
-    toRender = "loginPage";
-  } else {
-    toRender = "userSelection";
+  if (typeof userCount === "undefined") {
+    return (
+      <h1>
+        Loading<h1></h1>
+      </h1>
+    );
   }
+  if (userCount === 0) {
+    navigate("/registeruser");
+  }
+  if (userCount > 0) {
+    navigate("/selectuser");
+  }
+  // if (userCount === 0) {
+  //   toRender = "loginPage";
+  // } else {
+  //   toRender = "userSelection";
+  // }
 
-  return (
-    <>
-      {typeof userCount === "undefined" && <h1>Loading...</h1>}
-      {/* {toRender === "editPage" && <UserEdit />}/ */}
-      {toRender === "loginPage" && <LoginPage />}
-      {toRender === "userSelection" && <UserSelectionPage />}
-    </>
-  );
+  // return (
+  //   <>
+  //     {typeof userCount === "undefined" && <h1>Loading...</h1>}
+  //     {/* {toRender === "editPage" && <UserEdit />}/ */}
+  //     {toRender === "loginPage" && <LoginPage />}
+  //     {toRender === "userSelection" && <UserSelectionPage />}
+  //   </>
+  // );
 };
 
 export default UserPage;
