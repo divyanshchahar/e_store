5c2971f1873bfd9527a6f1f6ee7090306fc4cb82
divyanshchahar <divyanshchahar@outlook.com>
2022-08-19 20:53:10 +0530

    added createCartData

---
+++ src/services/utils/createCartData.js
@@ -0,0 +1,25 @@
+function createCartData(cartData, products) {
+  // array of product ids in cart
+  const temp1 = cartData.cartData.cartItems.map((item) => {
+    return item.pid;
+  });
+
+  // products filtered by ids of items in the cart
+  const temp2 = products.filter((item) => {
+    return temp1.includes(item.id);
+  });
+
+  var cartDisplay = []; //
+
+  temp2.map((itemA) => {
+    cartData.cartData.cartItems.map((itemB) => {
+      if (itemA.id === itemB.pid) {
+        cartDisplay.push({ ...itemA, qty: itemB.qty });
+      }
+    });
+  });
+
+  return cartDisplay;
+}
+
+export default createCartData;
