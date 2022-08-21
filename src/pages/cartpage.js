7d006ee1d5e7e5c0e9345380691c035391a40844
divyanshchahar <divyanshchahar@outlook.com>
2022-08-21 18:21:42 +0530

    removed unecessary code

--- src/pages/cartpage.js
+++ src/pages/cartpage.js
@@ -3,7 +3,6 @@ import useAPIData from "../services/utils/useAPIData";
 import CartItems from "../layouts/cartitems";
 
 const Cart = () => {
-  const temp = useAPIData;
   const [appData] = useAPIData("http://localhost:3001/appdata");
   const id = typeof appData === "undefined" ? null : appData.currentUser;
   const cartData = useAPIData(`http://localhost:3001/cart/${id}`);
