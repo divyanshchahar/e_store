d31a5184c2cbccb2ce52c75ca3bf6eaf82fc20c4
divyanshchahar <divyanshchahar@outlook.com>
2022-09-10 16:52:51 +0530

    changed cart page

--- src/pages/cartpage.js
+++ src/pages/cartpage.js
@@ -1,11 +1,30 @@
+//IMPORT REACT FUNCTIONALITY
+import { useEffect } from "react";
+import { useState } from "react";
+
 // IMPORTING COMPONENTS
-import useAPIData from "../services/utils/useAPIData";
+// import useAPIData from "../services/utils/useAPIData";
 import CartItems from "../layouts/cartitems";
 
 const Cart = () => {
-  const [appData] = useAPIData("http://localhost:3001/appdata");
-  const id = typeof appData === "undefined" ? null : appData.currentUser;
-  const cartData = useAPIData(`http://localhost:3001/cart/${id}`);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/appdata")
+      .then((response) => response.json())
+      .then(([userId]) => {
+        fetch(`http://localhost:3001/cart/${userId.currentUser}`)
+          .then((response) => response.json())
+          .then((actualData) => {
+            setCartData(actualData.cartItems);
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
       {cartData.length === 0 ? (
