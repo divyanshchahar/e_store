290b9d3c72c359addca4cc3a608115b132318ce3
divyanshchahar <divyanshchahar@outlook.com>
2022-09-12 12:23:56 +0530

    added function to retrieve datafrom api

--- src/middleware/checkout.js
+++ src/middleware/checkout.js
@@ -1,1 +1,16 @@
-export default function checkout() {}
+export default function checkout() {
+  // function to get data from api call
+  async function getData(url) {
+    try {
+      const response = await fetch(url);
+      if (response.status < 400) {
+        const data = await response.json();
+        return [data, "ok"];
+      } else {
+        return ["", "request failed"];
+      }
+    } catch {
+      return ["", "network error"];
+    }
+  }
+}
